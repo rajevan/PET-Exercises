9a8514e14014818e6f54148dfb9abff7bce7e504
rajevan <s.rajevan@gmail.com>
2019-01-11 10:28:22 +0000

    task 2 completed

--- Lab01Basics/Lab01Code.py
+++ Lab01Basics/Lab01Code.py
@@ -34,8 +34,11 @@ def encrypt_message(K, message):
     """ Encrypt a message under a key K """
 
     plaintext = message.encode("utf8")
+    aes = Cipher("aes-128-gcm")
+    iv = urandom(16)
+
     
-    ## YOUR CODE HERE
+    ciphertext, tag = aes.quick_gcm_enc(K, iv, plaintext)
 
     return (iv, ciphertext, tag)
 
@@ -44,7 +47,8 @@ def decrypt_message(K, iv, ciphertext, tag):
 
         In case the decryption fails, throw an exception.
     """
-    ## YOUR CODE HERE
+    aes = Cipher("aes-128-gcm")
+    plain = aes.quick_gcm_dec(K, iv, ciphertext, tag)
 
     return plain.encode("utf8")
 
